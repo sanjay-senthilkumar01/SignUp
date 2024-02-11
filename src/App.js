d21cfd53f88bb9a4d9d3234f94f04e890b326ef6
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2024-02-11 11:14:31 +0530

    commited

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+function App() {
+  return(
+  <div className="Frame4">
+  <img className="BackgroundImage2" src="Images\background images\background image2.png" alt="BackgroundImage"/>
+  <div className="SignUpFrame1"/>
+  <div className="SignUpFrame2"/>
+  <div className="Inverse">
+    <span className="SpanInverse">inverse</span>
+    <span className="SpanDot2" >.</span>
+    </div>
+  <img className="Logo"  src="Images\logo.png" alt ="Logo"/>
+  <div className="SignUp">
+    <span className="SpanSignUp">sign up</span>
+    <span className="SpanDot1" >.</span>
+    </div>
+  <div className="InverseQuote">inverse is much better when<br/>you have an account.</div>
+  <div className="Name">NAME</div>
+  <div className="NameText"/>
+  <div className="MailId">MAIL ID</div>
+  <div className="MailIdText"/>
+  <div className="Username" >USERNAME</div>
+  <div className="UsernameText"/>
+  <div className="Password">PASSWORD</div>
+  <div className="PasswordText"/>
+  <div className="ConfirmPassword">CONFIRM PASSWORD</div>
+  <div className="ConfirmPasswordText"/>
+  <div className="AgreeTerms">By Signing in I agree with terms and conditions</div>
+  <div className="TickBox"/>
+  <div className="SignUpBut"/>
+  <div className="SignUpButton">sign up</div>
+  <div className="LogInButton">log in</div>
+  <div className="Or">or</div>
+  <div className="Hello">
+    <span className="SpanHello">Hello</span>
+    <span className="SpanEx">!</span>
+    </div>
+  <div className="Back">back</div>
+</div>
+  );
+}
+
+export default App;
